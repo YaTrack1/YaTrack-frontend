6ae95fb1d35c9c647c7bb0c6bbf841d0a98858c9
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-23 21:24:43 +0700

    refactor: add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import { CreateVacancyFormSteps } from './components/shared/CreateVacancyFormSteps/CreateVacancyFormSteps';
 import Popup from './components/shared/Popup/Popup';
 import {Header} from './components/Header/Header';
+import { Card } from './components/shared/Card/Card';
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <CreateVacancyFormSteps />
-      <Popup />
+      <Popup /> */}
+      <Card/>
     </>
   );
 }
