c668521435ecc65474cea0704ec9111093a6e946
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-24 00:39:54 +0700

    feat: add markup for Card comp

--- src/components/shared/Card/Card.tsx
+++ src/components/shared/Card/Card.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
 import styles from './Card.module.scss';
+// заглушки
+import photo from '../../../vendor/images/candidate_photo_sample.png';
+import stats from '../../../vendor/images/data_sample.svg';
+//--------------------------------------------------------------------
 
 export const Card = () => {
-  return(
-    <section className={styles.card}>
+  // заглушки
+  const name = 'Сидорова Наталья';
+  const title = 'Junior';
+  const activity = 'Была(а) 1 час назад';
+  //--------------------------------------------------------------------
 
+  return (
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <p className={styles.header_title}>{name}</p>
+        <div className={styles.header_img_wrapper}>
+          <img className={styles.header_img} src={photo} alt='Фото кандидата'/>
+        </div>
+      </div>
+      <div className={styles.data}>
+        <img src={stats} alt='Статистика' />
+      </div>
+      <div className={styles.footer}>
+        <span className={styles.footer_title}>{title}</span>
+        <span className={styles.footer_subtitle}>{activity}</span>
+        <div className={styles.footer_buttons}>
+          {/* !!! тут кнопки */}
+        </div>
+      </div>
     </section>
   );
 };
