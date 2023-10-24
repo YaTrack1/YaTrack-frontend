998e2fba988546f084f25015ac740facf2fab6ed
YuliaMenachoPages <yumenachopages@gmail.com>
2023-10-24 13:27:28 +0300

    2 different forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { CreateVacancyFormSteps } from './components/shared/CreateVacancyFormSteps/CreateVacancyFormSteps';
-import Popup from './components/shared/Popup/Popup';
 import {Header} from './components/Header/Header';
-import { MuiLoginForm } from './components/MuiLoginForm/MuiLoginForm';
-import { AuthorizationWrapper } from './components/shared/AuthorizationWrapper/AuthorizationWrapper';
+import { MuiRegisterForm } from './components/MuiRegisterForm/MuiRegisterForm';
 import { MainMenu } from './components/MainMenu/MainMenu';
 import {MainPage} from './components/MainPage/MainPage';
+import { MuiLoginForm } from './components/MuiLoginForm/MuiLoginForm';
 
 function App() {
   return (
     <>
       <Header/>
       <CreateVacancyFormSteps />
-      <MuiLoginForm />
-      <AuthorizationWrapper header='Авторизация'/>
+      <MuiRegisterForm role='employer'/>
+      <MuiLoginForm role='employer' />
       <MainPage />
       {/* <MainMenu /> */}
       {/* <CreateVacancyFormSteps />
--- src/components/shared/AuthorizationWrapper/AuthorizationWrapper.tsx
+++ src/components/shared/AuthorizationWrapper/AuthorizationWrapper.tsx
@@ -1,21 +1,23 @@
-import { Stack } from '@mui/material';
-import BasicSelect from '../MuiSelectButton/MuiSelectButton';
-import styles from './AuthorizationWrapper.module.scss';
-import { MuiLoginForm } from '../../MuiLoginForm/MuiLoginForm';
-import { Link } from 'react-router-dom';
-
-type TFormValues = {
-  header: string
-}
-export function AuthorizationWrapper({ header }) {
-  return(
-    <div className={styles.authWrapper}>
-    <Stack spacing={2} width={392}>
-    <h1 className={styles.AuthorizationWrapper__header}>{header}</h1>
-    <BasicSelect />
-      <MuiLoginForm/>
-    </Stack>
-      <p className={styles.test}>Новый пользователь? <Link to='/'>Зарегистрироваться</Link></p>
-    </div>
-  )
-}
+// import { Stack } from '@mui/material';
+// import BasicSelect from '../MuiSelectButton/MuiSelectButton';
+// import styles from './AuthorizationWrapper.module.scss';
+// import { MuiRegisterForm } from '../../MuiRegisterForm/MuiRegisterForm';
+// import { Link } from 'react-router-dom';
+//
+// type TFormValues = {
+//   header: string
+// }
+// export function AuthorizationWrapper({ header, text, linkText }) {
+//   //Ю. Временный хардкод
+//   const role='employer'
+//   return(
+//     <div className={styles.authWrapper}>
+//     <Stack spacing={2} width={392}>
+//     <h1 className={styles.AuthorizationWrapper__header}>{header}</h1>
+//     <BasicSelect />
+//       <MuiRegisterForm role={role}/>
+//     </Stack>
+//       <p className={styles.test}>{text} <Link to='/{link}'>{linkText}</Link></p>
+//     </div>
+//   )
+// }
