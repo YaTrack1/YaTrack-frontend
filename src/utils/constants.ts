768c8124bba7fbfc953c7759287188546fb051f6
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-24 21:45:03 +0700

    feat: add consts for error pages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,3 +11,8 @@ export const METHOD = {
   PATCH: 'PATCH',
   DELETE: 'DELETE',
 };
+
+export const NOT_FOUND_TITLE = 'Страница не найдена';
+export const NOT_FOUND_SUBTITLE = 'Страница, которую вы запрашиваете, не существует. Возможно, она была удалена, или вы набрали неверный адрес';
+export const BAD_GATEWAY_TITLE = 'Отсутствует подключение к сети';
+export const BAD_GATEWAY_SUBTITLE = 'Проверьте соединение с интернетом или настройки брандмауэра. Для продолжения работы требуется доступ к сети Интернет';
