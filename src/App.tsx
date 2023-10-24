20996d7c7fa114932517fb61c90c03641b476ded
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-24 16:29:13 +0700

    fix: resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { CreateVacancyFormSteps } from './components/shared/CreateVacancyFormSteps/CreateVacancyFormSteps';
 import Popup from './components/shared/Popup/Popup';
 import {Header} from './components/Header/Header';
-import { Card } from './components/shared/Card/Card';
 
 function App() {
   return (
     <>
-      {/* <Header/>
+      <Header/>
       <CreateVacancyFormSteps />
-      <Popup /> */}
-      <Card/>
+      <Popup />
     </>
   );
 }
