a98819fb40af90a452ed3b5ca4e50fd265a9345b
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-24 21:47:29 +0700

    feat: add NotFoundPage component

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { ErrorPage } from '../shared/ErrorPage/ErrorPage';
+import image from '../../vendor/images/404_large_chart.png';
+import { NOT_FOUND_SUBTITLE, NOT_FOUND_TITLE } from '../../utils/constants';
+
+export const NotFoundPage = () => {
+  return(
+    <ErrorPage
+      title={NOT_FOUND_TITLE}
+      subtitle={NOT_FOUND_SUBTITLE}
+      imageSrc={image}
+    />
+  );
+};
