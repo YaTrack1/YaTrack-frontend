8c1ed6f2956a8b8debf4badb99d5c2ad88f21514
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-25 20:46:02 +0700

    refactor: change naming

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import {Header} from './components/Header/Header';
 import {MainPage} from './pages/MainPage/MainPage';
 import { NotFoundNotice } from './components/NotFoundNotice/NotFoundNotice';
 import { PublishedVacancies } from './pages/PublishedVacancies/PublishedVacancies';
-import { NoFavoritesNotice } from './components/NoFavoritesNotice/NoFavoritesNotice';
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Routes>
+      <Routes>
         <Route path='/signin' element={<p>Signin</p>}/>
         <Route path='/signup' element={<p>Signup</p>}/>
         <Route path='/' element={<Navigate to='/employer/home'/>}/>
@@ -17,8 +16,7 @@ function App() {
         <Route path='/employer/resumes' element={<MainPage />}/>
         <Route path='/employer/resumes/:id' element={<p>ResumeOneCard</p>}/>
         <Route path='*' element={<NotFoundNotice />}/>
-      </Routes> */}
-      <NoFavoritesNotice/>
+      </Routes>
     </>
   );
 }
