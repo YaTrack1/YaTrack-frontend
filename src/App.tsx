f88b1cbf1842963463bbe39dee1093183cdddcb2
YuliaMenachoPages <yumenachopages@gmail.com>
2023-10-25 18:08:01 +0300

    added theme pallet

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,31 @@ import {Header} from './components/Header/Header';
 import {MainPage} from './pages/MainPage/MainPage';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 import { PublishedVacancies } from './pages/PublishedVacancies/PublishedVacancies';
+import { createTheme, ThemeProvider, Button } from '@mui/material';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#FF0200',
+        light: '#B5B5B7',
+        dark: '#1D6BF3',
+        contrastText: '#FFFFFF',
+      },
+      secondary: {
+        main: '#FFFFFF',
+        light: '#1D6BF3',
+        dark: '#1D6BF3',
+        contrastText: '#B5B5B7',
+      },
+      error: {
+        main: '#FF0200',
+      },
+    },
+  })
   return (
+    <ThemeProvider theme={theme}>
     <>
       <Header />
       <Routes>
@@ -17,7 +39,9 @@ function App() {
         <Route path='/employer/resumes/:id' element={<p>ResumeOneCard</p>}/>
         <Route path='*' element={<NotFoundPage />}/>
       </Routes>
+      <Button />
     </>
+    </ThemeProvider>
   );
 }
 
