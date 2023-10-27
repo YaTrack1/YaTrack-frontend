ff1851db568adfd8c1cc73f8a70f6c75749c9458
YuliaDuk <yule444ka1234@mail.ru>
2023-10-27 10:15:49 +0300

    feat(mainpage): фильтрация подменю

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,118 +1,59 @@
 import { MainMenu } from '../../components/shared/MainMenu/MainMenu';
-import { Box } from '@mui/material';
-import { IconButton, Typography, ListItemText } from '@mui/material';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import Menu from '@mui/material/Paper';
-import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import {useState} from 'react';
-import Alert from '@mui/material/Alert';
-import Collapse from '@mui/material/Collapse';
-import { AlertModalPopup } from '../../components/shared/UI/AlertModalPopup/AlertModalPopup';
-import { Card } from '../../components/shared/Card/Card';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 import {FiltredVacancies} from '../../components/shared/FiltredVacancies/FiltredVacancies';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
+const CssButtonMenu = styled(Button) ({
+  padding: '0',
+  marginRight: '20px',
+  color: '#797981',
+  textTransform: 'none',
+  fontSize: '24px',
+  borderRadius: '0',
+  '&:hover': {
+    backgroundColor: 'white',
+    boxShadow: 'none',
+  },
+});
+
+const CssIconButton = styled(IconButton)({
+  '&:hover': {
+    filter: 'sepia(1) hue-rotate(170deg) saturate(100)',
+  },
+})
 export const MainPage: React.FC = () => {
-  const menuBtn = [{i: 1, menu: 'Подходящие'}, {i:2, menu: 'Избранное'}, {i: 3, menu:'Приглашенные'}]
+  const menuBtn = [{i: 1, menu: 'Подходящие кандидаты'}, {i:2, menu: 'Избранное'}, {i: 3, menu:'Приглашенные'}]
   const [activeMenu, setActiveMenu] = useState(menuBtn[0].i)
-  const [isActiveMenu, setIsActiveMenu] = useState(true);
-  const [isActiveBtn, setIsActiveBtn] = useState(false);
   const cardList=[{},{},{},{},{},{},{},{},{},{},{},{}];
-  const cardListButtons = ['Активные', 'Максимальное совпадение', 'Проявившие интерес'];
-  const likelistButtons = ['Принято', 'Ожидание', 'Отказ']
+  const cardListButtons = [{submenu: 'Активные', color: '#FFF9D3'}, {submenu: 'Максимальное совпадение', color: ' rgba(255, 191, 253, 0.87)'} , {submenu: 'Проявившие интерес', color: '#ACCCFF'}];
+  const likelistButtons = [{submenu: 'Принято', color: '#C2E5CE'}, {submenu: 'Отказ', color: '#FFDDE5'}, {submenu: 'Ожидание', color: '#FFF9D3'}]
   const likedList = [{},{},{}];
   const invitedList = [{},{}];
-  const Item = styled(Paper)(() => ({
-    height: '300px',
-    borderRadius: '12px',
-    boxShadow: '0px 4px 6px 0px rgba(176, 190, 197, 0.30)',
-  }));
-  const ButtonClassYellow = styled(Button) ({
-    border: '0',
-    color: 'black',
-    borderRadius: '12px',
-    textTransform: 'none',
-    fontSize: 16,
-    backgroundColor: `${isActiveBtn? '#DDE0E4' : '#FFF9D3'}`,
-  });
-  const MenuCandidatesItem = styled(MenuItem) ({
-    borderBottom: '1px solid white',
-    color: '#797981',
-    '&:hover': {
-      color: 'black',
-      backgroundColor: 'white',
-    },
-    '&:active': {
-      borderBottom: '1px solid #1D6BF3',
-      color: 'black',
-    },
-    '&:focus': {
-      borderBottom: '1px solid #1D6BF3',
-      color: 'black',
-    },
-  });
-  const CssButtonMenu = styled(Button) ({
-    color: '#797981',
-    textTransform: 'none',
-    fontSize: '24px',
-    borderRadius: '0',
-    '&:hover': {
-      backgroundColor: 'white',
-      boxShadow: 'none',
-    },
-  });
-  const [selectedValue, setSelectedValue] = useState('candidates');
 
-  // const handleChangeRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSelectedValue((event.target as HTMLInputElement).value);
-  // };
   function listCandidates () {
     if (activeMenu===menuBtn[0].i) return [cardList, cardListButtons];
     if (activeMenu===menuBtn[1].i) return [likedList, cardListButtons];
     else return [invitedList, likelistButtons];
-    // if (selectedValue==='candidates')
-    //   return [cardList, cardListButtons];
-    // if (selectedValue ==='liked')
-    //   return [likedList, cardListButtons];
-    // else return [invitedList, likelistButtons];
+
   }
+
   return (
     <Box sx={{display:'flex', pr: '48px'}}>
       <MainMenu />
       <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', flexGrow: 1}}>
-        <Box sx={{display: 'flex'}}>
-          <CssButtonMenu disableElevation disableRipple onClick={()=>{setActiveMenu(menuBtn[0].i)}} sx={{borderBottom: `${activeMenu===menuBtn[0].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[0].i ? 'black' : '#797981'}`}}>{menuBtn[0].menu}</CssButtonMenu>
-          <CssButtonMenu onClick={()=>{setActiveMenu(menuBtn[1].i)}} sx={{borderBottom: `${activeMenu===menuBtn[1].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[1].i ? 'black' : '#797981'}`}}>{menuBtn[1].menu}</CssButtonMenu>
-          <CssButtonMenu onClick={()=>{setActiveMenu(menuBtn[2].i)}} sx={{borderBottom: `${activeMenu===menuBtn[2].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[2].i ? 'black' : '#797981'}`}}>{menuBtn[2].menu}</CssButtonMenu>
-        </Box>
+        <Box sx={{display: 'flex', m:'40px 0 35px 24px'}}>
+          <CssIconButton disableRipple><ArrowBackIosIcon/></CssIconButton>
+          <Typography variant='h2' sx={{fontSize: '34px'}}>UX/UI дизайнер</Typography>
 
-        {/* <IconButton disableRipple sx={{mt:'30px', color: 'black'}}>
-          <ArrowBackIosNewIcon sx={{ml: '20px', mr: '15px'}}/>
-          <Typography variant='h4' component='div'>UX/UI дизайнер</Typography>
-        </IconButton>
-        <FormControl sx={{}}>
-          <RadioGroup
-            aria-labelledby='Меню кандидатов'
-            defaultValue='candidates'
-            name='radio-buttons-group'
-            onChange={handleChangeRadio}
-          >
-            <Box sx={{display: 'flex', ml: '30px', mt:'28px', gap: '40px'}}>
-              <FormControlLabel sx={{color: `${selectedValue==='candidates' ? 'black' : '#797981'}`, borderBottom: `${selectedValue==='candidates' ? '1px solid #1D6BF3' : '1px solid white'}`}} value='candidates' control={<Radio sx={{display: 'none'}}/>} label='Подходящие кандидаты'/>
-              <FormControlLabel sx={{color: `${selectedValue==='liked' ? 'black' : '#797981'}`, borderBottom: `${selectedValue==='liked' ? '1px solid #1D6BF3' : '1px solid white'}`}} value='liked' control={<Radio sx={{display: 'none'}}/>} label='Избранные'  />
-              <FormControlLabel sx={{color: `${selectedValue==='invited' ? 'black' : '#797981'}`, borderBottom: `${selectedValue==='invited' ? '1px solid #1D6BF3' : '1px solid white'}`}}  value='invited' control={<Radio sx={{display: 'none'}}/>} label='Приглашенные'/>
-            </Box>
-          </RadioGroup>
-        </FormControl> */}
-        <FiltredVacancies selectedValue={selectedValue} vacanciesList={listCandidates()}/>
+        </Box>
+        <Box sx={{display: 'flex',ml: '24px'}}>
+          <CssButtonMenu disableElevation disableRipple onClick={()=>{setActiveMenu(menuBtn[0].i)}} sx={{borderBottom: `${activeMenu===menuBtn[0].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[0].i ? 'black' : '#797981'}`}}>{menuBtn[0].menu}</CssButtonMenu>
+          <CssButtonMenu disableElevation disableRipple onClick={()=>{setActiveMenu(menuBtn[1].i)}} sx={{borderBottom: `${activeMenu===menuBtn[1].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[1].i ? 'black' : '#797981'}`}}>{menuBtn[1].menu}</CssButtonMenu>
+          <CssButtonMenu  disableElevation disableRipple onClick={()=>{setActiveMenu(menuBtn[2].i)}} sx={{borderBottom: `${activeMenu===menuBtn[2].i ? '1px solid #1D6BF3' : 'white'}`, color: `${activeMenu===menuBtn[2].i ? 'black' : '#797981'}`}}>{menuBtn[2].menu}</CssButtonMenu>
+        </Box>
+        <FiltredVacancies activeMenu={activeMenu} vacanciesList={listCandidates()}/>
       </Box>
     </Box>
   );
