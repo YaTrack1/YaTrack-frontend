2e7830a9e00f7c1fe62613af9701182c26ea7d8b
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-27 14:02:18 +0700

    feat: create hard code variables for the card

--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -1,26 +1,26 @@
 import { useState } from 'react';
 import styles from './ExperienceCard.module.scss';
 
-interface ExperienceCardProps {
-  title: string;
-  period: string;
-  experince: string;
-  duties: string;
-}
+// interface ExperienceCardProps {
+//   title: string;
+//   period: string;
+//   experince: string;
+//   duties: string;
+// }
 
 export const ExperienceCard = () => {
   const [ isClicked, setIsClicked ] = useState(false);
   const containerClass = `${styles.container} ${isClicked && styles.containerLong}`;
   const hiddenBlockClass = `${styles.text} ${!isClicked && styles.hidden_block}`;
   const buttonClass = `${styles.arrow_down} ${isClicked && styles.arrow_up}`;
+
   const toggleButton = () => setIsClicked(!isClicked);
 
   // будущие пропсы, а сейчас заглушки
   const title = 'Графический дизайнер';
   const period = 'январь 2020 - январь 2022';
   const experince = '2 года';
-  const duties = '1. Исследование и анализ целевой аудитории, конкурентов и отрасли.';
-  //--------------------------------------------------------------------
+  const duties: string = '1. Исследование и анализ целевой аудитории, конкурентов и отрасли. 2. Разработка концепций и идей для визуальных решений, включая логотипы, типографику, изображения, цвета, шаблоны и т.д. 3. Создание макетов и прототипов для печати и цифровых медиа.';
 
   return(
     <div className={containerClass}>
