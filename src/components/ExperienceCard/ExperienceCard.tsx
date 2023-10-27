29ee0ec653f301cc2a9b82a7dd9ce324d718d72f
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-27 16:55:13 +0700

    style: add margin-bottom and white-space: normal

--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -3,12 +3,17 @@ import styles from './ExperienceCard.module.scss';
 
 interface ExperienceCardProps {
   title: string;
-  // period: string;
-  // experince: string;
-  // duties: string;
+  period: string;
+  experince: string;
+  duties: string;
 }
 
-export const ExperienceCard = ({title}: ExperienceCardProps) => {
+export const ExperienceCard = ({
+  title,
+  period,
+  experince,
+  duties,
+}: ExperienceCardProps) => {
   const [ isClicked, setIsClicked ] = useState(false);
   const containerClass = `${styles.container} ${isClicked && styles.containerLong}`;
   const hiddenBlockClass = `${styles.text} ${!isClicked && styles.hidden_block}`;
@@ -18,9 +23,9 @@ export const ExperienceCard = ({title}: ExperienceCardProps) => {
 
   // будущие пропсы, а сейчас заглушки
   // const title = 'Графический дизайнер';
-  const period = 'январь 2020 - январь 2022';
-  const experince = '2 года';
-  const duties: string = '1. Исследование и анализ целевой аудитории, конкурентов и отрасли. 2. Разработка концепций и идей для визуальных решений, включая логотипы, типографику, изображения, цвета, шаблоны и т.д. 3. Создание макетов и прототипов для печати и цифровых медиа.';
+  // const period = 'январь 2020 - январь 2022';
+  // const experince = '2 года';
+  // const duties: string = '1. Исследование и анализ целевой аудитории, конкурентов и отрасли. 2. Разработка концепций и идей для визуальных решений, включая логотипы, типографику, изображения, цвета, шаблоны и т.д. 3. Создание макетов и прототипов для печати и цифровых медиа.';
 
   return(
     <div className={containerClass}>
